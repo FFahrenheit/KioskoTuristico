c08c78bf031b47986f2f50b47ee9e3fe9e0c396f
Iván L. Murillo <diskman199@gmail.com>
2021-07-30 22:02:25 -0500

    Build de electron
    Arreglando bugs

---
+++ main.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron');
+const url = require("url");
+const path = require("path");
+
+let win;
+
+let createWindow = () => {
+
+    win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        backgroundColor: '#ffffff',
+        icon: url.format({
+            pathname: path.join(__dirname, `/dist/assets/icon.png`),
+            protocol: "file:",
+            slashes: true
+          }) 
+        //`file://${__dirname}/dist/assets/icon.png`
+    });
+
+    win.loadURL(
+        url.format({
+          pathname: path.join(__dirname, `/dist/index.html`),
+          protocol: "file:",
+          slashes: true
+        })
+    );
+
+
+    win.on('closed', () => {
+        win = null
+    });
+}
+
+
+app.on('ready', createWindow); 
+
+app.on('window-all-closed', () =>{
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate',()=>{
+    if(win === null){
+        createWindow();
+    }
+});
